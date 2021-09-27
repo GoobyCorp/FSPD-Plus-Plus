eeadb5888d2d7fd178fc82d00d3821c523142bf5
John Dong <pls@gooby.co>
2021-09-27 02:43:44 -0400

    added build script and added types.hpp

--- src/include/stdafx.hpp
+++ src/include/stdafx.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
+// lib
+#include "types.hpp"
---
+++ src/include/types.hpp
@@ -0,0 +1,6 @@
+#ifndef _TYPES_HPP
+#define _TYPES_HPP
+typedef unsigned char BYTE, uint8, u8;
+
+typedef BYTE* PBYTE;
+#endif
