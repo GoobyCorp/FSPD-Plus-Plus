5b460ed4d522a83a57349e87f43daa62332b165d
John Dong <pls@gooby.co>
2021-09-28 02:57:15 -0400

    Added first receive code

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ int main(int argc, char* argv[]) {
 
 	int srvSock;
 	int opt = TRUE;
-	sockaddr_in addr;
-	memset((PBYTE)&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(SERVER_PORT);
-	addr.sin_addr.s_addr = Utils::AddressToLong((const PCHAR)SERVER_ADDR);
+	sockaddr_in srvAddr;
+	memset((PBYTE)&srvAddr, 0, sizeof(srvAddr));
+	srvAddr.sin_family = AF_INET;
+	srvAddr.sin_port = htons(SERVER_PORT);
+	srvAddr.sin_addr.s_addr = Utils::AddressToLong((const PCHAR)SERVER_ADDR);
 
 	if((srvSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
 		perror("socket creation failed!\n");
@@ -32,10 +32,18 @@ int main(int argc, char* argv[]) {
 		return ERROR_SETSOCKOPT_FAILED;
 	}
 
-	if(bind(srvSock, (sockaddr*)&addr, sizeof(addr))) {
+	if(bind(srvSock, (sockaddr*)&srvAddr, sizeof(srvAddr))) {
 		perror("socket bind failed!\n");
 		return ERROR_SOCKET_BIND_FAILED;
 	}
+
+	// receive variables
+	int len, n;
+	BYTE buf[FSP_MAXSPACE];
+	memset(buf, 0, FSP_MAXSPACE);
+
+	sockaddr_in cliAddr;
+	n = recvfrom(srvSock, buf, FSP_MAXSPACE, 0, (sockaddr*)&cliAddr, (socklen_t*)&len);
 
 	return ERROR_NONE;
 }
